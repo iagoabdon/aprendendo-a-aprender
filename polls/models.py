58ac0291ca963a77ef5790451841591941fa8057
Iago <iago.0206@hotmail.com>
2025-03-25 17:37:24 -0300

    'att'

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,29 @@
 from django.db import models
+import datetime
 
-class question(models.Model):
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
     question_text = models.CharField(max_length=100)
     pub_date = models.CharField(max_length=100)
 
+    
+    def __str__(self):
+      return self.question_text
 
-class choice (models.Model):
-    question = models.ForeignKey(question, on_delete=models.CASCADE)
+class Choice (models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+  
+
+
+
+
+
